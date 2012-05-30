dd02af2f66b7d2ecaa1693406b1f0f5ce879b15e
Romain Yon <romain.yon@gmail.com>
2012-05-30 22:51:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudGephi::Application.config.secret_token = '5e7f5eacf8ef22fb7792f2727f6baaac468b04ecbafd1d3a60671587d8e9a1037270b8967c753b72a7d4efff28b321566b44e9dc6bb24562c43fe3db18bc1fe1'
